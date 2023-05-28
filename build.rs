ba59f89c6c6d2ba05007b1c1bd59f4ebf49e7f3f
wr7 <d-wr7@outlook.com>
2023-05-28 09:26:11 -0500

    Remove xcb

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use cfg_aliases::cfg_aliases;
 fn main() {
     cfg_aliases! {
         x11: {all(unix, feature="x11")},
-        xcb: {all(unix, feature="xcb")},
         win32: {windows},
     }
 }

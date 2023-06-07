5b015a09712e9c453d5d099f4aded75a7d0fa0dd
wr7 <d-wr7@outlook.com>
2023-06-07 13:35:28 -0500

    Remove CoreWindow and fix leak

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,4 @@
-use crate::{state::CoreStateData, Color, RectRegion, WWindState, Window};
+use crate::{state::CoreStateData, Color, RectRegion, WWindState};
 
 mod core_state_implementation;
 
@@ -53,80 +53,80 @@ impl CoreDrawingContext {
     }
 }
 
-/// Represents a window.
-/// Note: this doesn't destroy the window upon drop
-#[derive(Clone)]
-pub struct CoreWindow {
-    pub core_window_ref: CoreWindowRef,
-    pub core_state_data: *mut CoreStateData,
-}
+// /// Represents a window.
+// /// Note: this doesn't destroy the window upon drop
+// #[derive(Clone)]
+// pub struct CoreWindow {
+//     pub core_window_ref: CoreWindowRef,
+//     pub core_state_data: *mut CoreStateData,
+// }
 
-impl PartialEq for CoreWindow {
-    fn eq(&self, other: &Self) -> bool {
-        self.core_window_ref == other.core_window_ref
-    }
-}
+// impl PartialEq for CoreWindow {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.core_window_ref == other.core_window_ref
+//     }
+// }
 
-impl CoreWindow {
-    /// Unsafe: function can only be called while the CoreState exists and on the thread where it was created
-    pub unsafe fn schedule_window_destruction(&mut self) {
-        let core_window_ref = self.core_window_ref;
-        let windows_to_destroy = &mut self.get_core_state_data_mut().windows_to_destroy;
+// impl CoreWindow {
+//     /// Unsafe: function can only be called while the CoreState exists and on the thread where it was created
+//     pub unsafe fn schedule_window_destruction(&mut self) {
+//         let core_window_ref = self.core_window_ref;
+//         let windows_to_destroy = &mut self.get_core_state_data_mut().windows_to_destroy;
 
-        if !windows_to_destroy.contains(&core_window_ref) {
-            windows_to_destroy.push(core_window_ref);
-        }
-    }
+//         if !windows_to_destroy.contains(&core_window_ref) {
+//             windows_to_destroy.push(core_window_ref);
+//         }
+//     }
 
-    pub fn get_core_state_data_mut(&mut self) -> &mut CoreStateData {
-        unsafe { &mut *self.core_state_data }
-    }
+//     pub fn get_core_state_data_mut(&mut self) -> &mut CoreStateData {
+//         unsafe { &mut *self.core_state_data }
+//     }
 
-    pub fn get_core_state_data(&self) -> &CoreStateData {
-        unsafe { &*self.core_state_data }
-    }
+//     pub fn get_core_state_data(&self) -> &CoreStateData {
+//         unsafe { &*self.core_state_data }
+//     }
 
-    pub fn on_window_close_attempt<
-        F: for<'a> FnMut(&'a mut WWindState, &'a mut Window) + 'static,
-    >(
-        &mut self,
-        closure: F,
-    ) {
-        let window_ref = self.core_window_ref;
+//     pub fn on_window_close_attempt<
+//         F: for<'a> FnMut(&'a mut WWindState, &'a mut Window) + 'static,
+//     >(
+//         &mut self,
+//         closure: F,
+//     ) {
+//         let window_ref = self.core_window_ref;
 
-        self.get_core_state_data_mut()
-            .windows
-            .get_mut(&window_ref)
-            .map(|data| data.on_close = Some(Box::new(closure)));
-    }
+//         self.get_core_state_data_mut()
+//             .windows
+//             .get_mut(&window_ref)
+//             .map(|data| data.on_close = Some(Box::new(closure)));
+//     }
 
-    pub fn on_redraw<F: for<'a> FnMut(&'a mut WWindState, &'a mut Window, RectRegion) + 'static>(
-        &mut self,
-        closure: F,
-    ) {
-        let window_ref = self.core_window_ref;
+//     pub fn on_redraw<F: for<'a> FnMut(&'a mut WWindState, &'a mut Window, RectRegion) + 'static>(
+//         &mut self,
+//         closure: F,
+//     ) {
+//         let window_ref = self.core_window_ref;
 
-        self.get_core_state_data_mut()
-            .windows
-            .get_mut(&window_ref)
-            .map(|data| data.redraw = Some(Box::new(closure)));
-    }
+//         self.get_core_state_data_mut()
+//             .windows
+//             .get_mut(&window_ref)
+//             .map(|data| data.redraw = Some(Box::new(closure)));
+//     }
 
-    pub fn get_drawing_context(&mut self) -> CoreDrawingContext {
-        let window_ref = self.core_window_ref;
-        let core_state = &mut self.get_core_state_data_mut().core_state;
+//     pub fn get_drawing_context(&mut self) -> CoreDrawingContext {
+//         let window_ref = self.core_window_ref;
+//         let core_state = &mut self.get_core_state_data_mut().core_state;
 
-        unsafe {
-            CoreDrawingContext {
-                context: core_state.get_context(window_ref),
-                core_state_data: self.core_state_data,
-            }
-        }
-    }
+//         unsafe {
+//             CoreDrawingContext {
+//                 context: core_state.get_context(window_ref),
+//                 core_state_data: self.core_state_data,
+//             }
+//         }
+//     }
 
-    pub fn get_size(&self) -> (u16, u16) {
-        self.get_core_state_data()
-            .core_state
-            .get_size(self.core_window_ref)
-    }
-}
+//     pub fn get_size(&self) -> (u16, u16) {
+//         self.get_core_state_data()
+//             .core_state
+//             .get_size(self.core_window_ref)
+//     }
+// }
--- src/util.rs
+++ src/util.rs
@@ -1,39 +1,5 @@
-use std::{
-    marker::PhantomData,
-    mem::MaybeUninit,
-    ops::{Deref, DerefMut},
-    rc::Rc,
-};
+use std::{marker::PhantomData, rc::Rc};
 #[derive(Default, Clone, Copy)]
 pub struct PhantomUnsend {
     _phantomdata: PhantomData<Rc<()>>,
-}
-
-/// A type that guarentees that `T` will be [forgotten](std::mem::forget) before `'a`.
-pub struct ForgetGuard<'a, T> {
-    inner: MaybeUninit<T>,
-    _phantomdata: PhantomData<&'a T>,
-}
-
-impl<'a, T> ForgetGuard<'a, T> {
-    pub fn new(inner: T) -> Self {
-        Self {
-            inner: MaybeUninit::new(inner),
-            _phantomdata: Default::default(),
-        }
-    }
-}
-
-impl<'a, T> Deref for ForgetGuard<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { self.inner.assume_init_ref() }
-    }
-}
-
-impl<'a, T> DerefMut for ForgetGuard<'a, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { self.inner.assume_init_mut() }
-    }
 }
